9bcdc0f04c99eff22461d09d64a8a07536b527f8
Huraira Shaikh <hurairashiekh076@gmail.com>
2026-03-03 06:58:11 +0500

    Another pattern added

--- inverted_triangle.dart
+++ inverted_triangle.dart
@@ -2,6 +2,10 @@ import 'dart:io';
 
 void main() {
   int n = 4;
+  inverted_number(n);
+}
+
+void inverted_number(int n) {
   for (var i = 0; i < n; i++) {
     for (int j = 0; j < i; j++) {
       stdout.write(" ");
