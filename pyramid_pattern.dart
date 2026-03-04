2501dd2c7146575f52fe3057358fc0cd2408807a
Huraira Shaikh <hurairashiekh076@gmail.com>
2026-03-04 09:40:16 +0500

    number pyramid Pattern

--- pyramid_pattern.dart
+++ pyramid_pattern.dart
@@ -3,28 +3,10 @@ import 'dart:io';
 void main() {
   int n = 4;
 // number_pyramid_pattern(n);
-  character_pyramids_pattern(n);
+ // character_pyramids_pattern(n);
 }
 
-void character_pyramids_pattern(int n) {
-  for (var i = 0; i < n; i++) {
-    int char = 'A'.codeUnitAt(0);
-    for (int j = 0; j < n - i - 1; j++) {
-      stdout.write(" ");
-    }
-    for (int j = 1; j <= i + 1; j++) {
-      stdout.write(String.fromCharCode(char));
-      char += 1;
-    }
-    char -= 2;
-    for (int j = i; j > 0; j--) {
-      stdout.write(String.fromCharCode(char));
-      char -= 1;
-    }
 
-    stdout.write("\n");
-  }
-}
 
 void number_pyramid_pattern(int n) {
   for (var i = 0; i < n; i++) {
