e02d53621ec6663716846daea77d101f27cd92a2
Huraira Shaikh <hurairashiekh076@gmail.com>
2026-03-05 08:48:42 +0500

    Character pyramid Pattern

--- pyramid_pattern.dart
+++ pyramid_pattern.dart
@@ -26,4 +26,18 @@ void character_pyramids_pattern(int n) {
   }
 }
 
+void number_pyramid_pattern(int n) {
+  for (var i = 0; i < n; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      stdout.write(" ");
+    }
+    for (int j = 1; j <= i + 1; j++) {
+      stdout.write(j);
+    }
+    for (int j = i; j > 0; j--) {
+      stdout.write(j);
+    }
 
+    stdout.write("\n");
+  }
+}
